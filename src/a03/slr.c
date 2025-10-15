ce9b8b58efdadfdf5769402bdbf12d5af3f4e6f3
ybertoldi <yuri.bertoldi003@gmail.com>
2025-10-14 21:33:41 -0300

    slr funcionando

--- src/a03/slr.c
+++ src/a03/slr.c
@@ -1,4 +1,8 @@
 #include "../../includes/tokenizer.h"
+#include "../../includes/common.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
   long target;
@@ -24,6 +28,23 @@ typedef enum {
   VAR_CONST,
   VAR_NUM_VARS
 } VARTYPE;
+
+char * var2str(VARTYPE v){
+  switch (v) {
+     case VAR_DEC:   return "VAR_DEC";
+     case VAR_EXPR:  return "VAR_EXPR";
+     case VAR_EXPR1: return "VAR_EXPR1";
+     case VAR_EXPR2: return "VAR_EXPR2";
+     case VAR_EXPR3: return "VAR_EXPR3";
+     case VAR_EXPR4: return "VAR_EXPR4";
+     case VAR_EXPR5: return "VAR_EXPR5";
+     case VAR_TIPO:  return "VAR_TIPO";
+     case VAR_CONST: return "VAR_CONST";
+     default: exit(1);
+  }
+}
+
+#define START_SYMBOL VAR_DEC
 
 long goto_table[39][VAR_NUM_VARS] = {
     [1] = {[VAR_TIPO] = 2, [TKTYPE_T_INT] = 3, [TKTYPE_T_CHAR] = 4, [TKTYPE_T_BOOL] = 5, [TKTYPE_T_LONG] = 6, [TKTYPE_T_FLOAT] = 7, [TKTYPE_T_DOUBLE] = 8},
@@ -116,5 +137,85 @@ Production reduce[39] = {
     [38] = PROD(VAR_EXPR5,      TKTYPE_OPPAREN, VAR_EXPR, TKTYPE_CLPAREN),
 };
 
+#define STACK_SIZE 4096
+long state_stack[STACK_SIZE];
+long stp = 0;
+void state_push(long v) { assert(stp < STACK_SIZE); state_stack[stp++] = v; }
+long state_pop(void)    { assert(stp > 0);          return state_stack[--stp]; }
+long state_peek(void)   { assert(stp > 0);          return state_stack[stp - 1]; }
+
+long symbols_stack[STACK_SIZE];
+long syp = 0;
+void symbol_push(long v) { assert(syp < STACK_SIZE); symbols_stack[syp++] = v; }
+long symbol_pop(void)    { assert(syp > 0);          return symbols_stack[--syp]; }
+long symbol_peek(void)   { assert(syp > 0);          return symbols_stack[syp - 1]; }
 
 
+void printstate(long t){
+  printf("STATES = {");
+  for (int i = 0; i < stp; i++) {
+    printf("%ld, ", state_stack[i]);
+  }
+  printf("}\t");
+
+  printf("SYMBOLS = {");
+  for (int i = 0; i < syp; i++) {
+    printf("%s, ", (symbols_stack[i] >= TKTYPE_NUM_TOKENS) ? var2str(symbols_stack[i]) : type2str(symbols_stack[i]));
+  }
+  printf("}\t");
+  printf("INPUT = %s\n", type2str(t));
+}
+
+int main() {
+  char buf[BUFSIZ];
+
+  printf("input > ");
+  while (readline(buf, BUFSIZ)) {
+    state_push(1);
+    TokenList *tklist = tokenize(buf); // TODO: funcao para limpar os tokens
+    int parse = 2;
+    long gt_state;
+    while (parse) {
+      if (parse == 2) printstate(tklist->type);
+
+      if (tklist && (gt_state = goto_table[state_peek()][tklist->type]) != 0) {
+        symbol_push(tklist->type);
+        state_push(gt_state);
+        tklist = tklist->next;
+      } else {
+        Production p = reduce[state_peek()];
+        if (p.target == 0) {
+          fprintf(stderr, "syntax error\n");
+          exit(1);
+        }
+
+        for (int i = p.num_elems - 1; i >= 0; i--) {
+          if (p.elements[i] != symbol_pop()) {
+            fprintf(stderr, "syntax error\n");
+            exit(1);
+          }
+          state_pop();
+        }
+
+        symbol_push(p.target);
+        if ((gt_state = goto_table[state_peek()][p.target]) != 0) {
+          state_push(gt_state);
+        }
+      }
+
+      parse -= (tklist == NULL)? 1 : 0;
+    }
+
+    printstate(0);
+    if (symbol_peek() == START_SYMBOL){
+      printf("ACC\n\n");
+    } else {
+      printf("ERR\n\n");
+    }
+    printf("final state: %ld\n", state_pop());
+    printf("input > ");
+    syp = 0;
+    stp = 0;
+  }
+  return 0;
+}
